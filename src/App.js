4204c58888e730681b94f43593ef473d73f76e15
Matt Mullins <matt.mullins@live.com>
2023-06-29 13:26:37 +0100

    react p5.js example

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Sketch from 'react-p5';
+import React from 'react';
+
+class App extends React.Component {
+	y = 0;
+	direction = '^';
+
+	setup = (p5, parentRef) => {
+		p5.createCanvas(600, 600).parent(parentRef);
+	};
+
+	draw = (p5) => {
+		p5.background(0);
+		p5.fill(255, this.y * 1.3, 0);
+		p5.ellipse(p5.width / 2, this.y, 50);
+		if (this.y > p5.height) this.direction = '';
+		if (this.y < 0) {
+			this.direction = '^';
+		}
+		if (this.direction === '^') this.y += 8;
+		else this.y -= 4;
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<h1>react-p5</h1>
+				<Sketch setup={this.setup} draw={this.draw} />
+			</div>
+		);
+	}
+}
+
+
+export default App;
+
